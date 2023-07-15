568a7a3f0fbd94f744c4fa7694f1abff1cf88976
mississppi <msspstudio@gmail.com>
2023-07-15 17:22:39 +0900

    0715

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,17 @@ import {faMoon, faSun} from '@fortawesome/free-solid-svg-icons'
 import {useKey} from 'react-use';
 import './Main.css'
 
-const Main = ({activeNote, onInputChange, onTextAreaChange, onUpdateNote, logout, isDarkMode, handleModeToggle, handleTitleFocus, handleTitleBlur}) => {
+const Main = ({
+  activeNote, 
+  onInputChange, 
+  onTextAreaChange, 
+  onUpdateNote, 
+  logout, 
+  isDarkMode, 
+  handleModeToggle, 
+  }) => {
+
+  const [isContentFocused, setIsContentFocused] = useState(false);
 
   const handleSave = () => {
     event.preventDefault();
@@ -14,6 +24,62 @@ const Main = ({activeNote, onInputChange, onTextAreaChange, onUpdateNote, logout
   }
   const saveKey = (event) => event.key === 's' && event.metaKey;
   useKey(saveKey, handleSave);
+
+
+  const handleCopy = (event) => {
+    if(isContentFocused) {
+
+      // これでコピーは止められる
+      event.preventDefault();
+
+      //cmd + c をしたキャレット位置を取得
+      const currentCaret = event.target.selectionStart;
+
+      //jsでstartとend取得
+      //文字列
+      const text = event.target.value;
+      const start = text.lastIndexOf('\n', currentCaret - 1) + 1;
+      const end = text.indexOf('\n', currentCaret);
+      const previous = text.charAt(currentCaret - 1);
+
+      //一致してかつ前の文字が改行コードじゃなければ
+      // const previous = text.charAt(currentCaret - 1);
+      if (currentCaret == text.length){
+        if(previous == '\n'){
+          console.log('こんなことしてちゃだめだ！');
+          return;
+        }
+      }
+
+      event.target.setSelectionRange(start, end === -1 ? text.length : end);
+      const selectedText = event.target.value.substring(start, end);
+      //copy
+      navigator.clipboard.writeText(selectedText)
+      .then(() => {
+        console.log("copieeeddd");
+        event.target.setSelectionRange(currentCaret, currentCaret);
+        return;
+      })
+      .catch((error) => {
+        console.log("osietetet");
+      })
+
+    }
+    // console.log("copyyy");
+    // console.log(isContentFocused);
+  }
+
+  const copyKey = (event) => event.metaKey && event.key === 'c';
+  useKey(copyKey, handleCopy);
+
+
+  const handleContentFocus = () => {
+    setIsContentFocused(true);
+  }
+
+  const handleContentBlur = () => {
+    setIsContentFocused(false);
+  }
 
   if(!activeNote){
     return <div className='no-active-note'>ノートを選んでね</div>
@@ -38,17 +104,20 @@ const Main = ({activeNote, onInputChange, onTextAreaChange, onUpdateNote, logout
           type="text" 
           value={activeNote?.title || ""}
           onChange={onInputChange}
-          onFocus={handleTitleFocus} onBlur={handleTitleBlur}
         />
         <textarea 
           id="content"
           className={`${isDarkMode ? "darkmode" : ""}`}
           value={activeNote?.content || ""}
           onChange={onTextAreaChange}
+          onFocus={handleContentFocus} onBlur={handleContentBlur}
         />
         <div className='app-main-help'>
           <span className='save'>SAVE = ⌘ + s</span>
         </div>
+        <button onClick={handleCopy}>
+          1行コピー
+        </button>
       </div>
     </div>
   )
